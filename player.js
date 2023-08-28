e13029499a106a7f1c81bac15b5e648345ea5019
ikeracevedo24 <84978577+ikeracevedo24@users.noreply.github.com>
2023-08-28 17:27:29 -0500

    e

--- player.js
+++ player.js
@@ -1,3 +1,41 @@
+(async ( ) => {
+    let sidebar = document.querySelector("#sidebar-small");
+    let peticion = await fetch("data.json");
+    let res = await peticion.json();
+    sidebarInfo = res.sidebarsmall;
+    console.log(sidebarInfo);
+    sidebar.insertAdjacentHTML('beforeend', /*html*/`
+        ${(sidebarInfo).map(value => /*html*/ `
+            <div class="sidebar-small-btn">
+                <a href="#">
+                    ${value.svg}
+                    ${value.span}
+                </a>
+            </div>
+        `).join('')}
+    </div>`
+    );
+    sidebar = document.querySelector("#sidebar-main");
+    sidebarInfo = res.sidebar;
+    for (let i = 1; i < 6; i++) {
+        if (i !== 3) {
+            sidebar.insertAdjacentHTML('beforeend', /*html*/`
+                <div class="side${i} sidebar-box">
+                    ${sidebarInfo[0][`side${i}`].map(value => /*html*/ `
+                        <div>
+                            <a href="#">
+                                ${value.svg}
+                                ${value.span}
+                            </a>
+                        </div>
+                    `).join('')}
+                </div>`
+            );
+        } else {
+            sidebar.insertAdjacentHTML('beforeend', /*html*/`${sidebarInfo[0].side3.html}`);
+        }
+    }
+}) ( );
 // FUNCION ABRIR SIDEBAR
 function sidebarOpen() {
     const sidebarContainer = document.querySelector("#sidebar-container");
