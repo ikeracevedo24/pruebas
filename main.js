83683dfdea2e2149fcf81c692dd5b16428e42e6d
ikeracevedo24 <84978577+ikeracevedo24@users.noreply.github.com>
2023-08-28 17:20:32 -0500

    e

--- main.js
+++ main.js
@@ -1,32 +1,49 @@
-let hola = async () => {
+(async ( ) => {
     const sidebar = document.querySelector("#sidebar-main");
-    try {
-        let peticion = await fetch(`json/data.json`); // Ruta relativa al archivo JSON
-        let res = await peticion.json();
-        sidebarInfo = res.sidebar;
-        console.log(sidebarInfo);
-        for (let i = 1; i < 6; i++) {
-            if (i !== 3) {
-                sidebar.insertAdjacentHTML('beforeend', /*html*/`
-                    <div class="side${i} sidebar-box">
-                        ${sidebarInfo[0][`side${i}`].map(value => /*html*/ `
-                            <div>
-                                <a href="#">
-                                    ${value.svg}
-                                    ${value.span}
-                                </a>
-                            </div>
-                        `).join('')}
-                    </div>`
-                );
-            } else {
-                sidebar.insertAdjacentHTML('beforeend', /*html*/`${sidebarInfo[0].side3.html}`);
-            }
+    console.log(sidebar)
+    let peticion = await fetch("data.json");
+    let res = await peticion.json();
+    sidebarInfo = res.sidebar;
+    console.log(sidebarInfo);
+    for (let i = 1; i < 6; i++) {
+        if (i !== 3) {
+            sidebar.insertAdjacentHTML('beforeend', /*html*/`
+                <div class="side${i} sidebar-box">
+                    ${sidebarInfo[0][`side${i}`].map(value => /*html*/ `
+                        <div>
+                            <a href="#">
+                                ${value.svg}
+                                ${value.span}
+                            </a>
+                        </div>
+                    `).join('')}
+                </div>`
+            );
+        } else {
+            sidebar.insertAdjacentHTML('beforeend', /*html*/`${sidebarInfo[0].side3.html}`);
         }
-    } catch (error) {
-        console.error("Error fetching data:", error);
     }
-};
-hola();
+}) ( );
+
+// (async ( ) => {
+//     const sidebar = document.querySelector("#sidebar-small");
+//     let peticion = await fetch("data.json");
+//     let res = await peticion.json();
+//     sidebarInfo = res.sidebarsmall;
+//     console.log(sidebarInfo);
+//     sidebar.insertAdjacentHTML('beforeend', /*html*/`
+//         ${(sidebarInfo).map(value => /*html*/ `
+//             <div class="sidebar-small-btn">
+//                 <a href="#">
+//                     ${value.svg}
+//                     ${value.span}
+//                 </a>
+//             </div>
+//         `).join('')}
+//     </div>`
+//     );
+// }) ( );
+
+
 
 console.log("JAJAJAJ")
